791517759fbf98a334bb32623eb7251e44506338
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2026-06-02 02:30:18 +0500

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,10 +2,18 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api || https://narrato-frontend-two.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api" || "https://blog-app-backend-two-nu.vercel.app/api",
   withCredentials: true, // MANDATORY for cookies/sessions
 });
+// Aise hona chahiye
+const BASE_URL = import.meta.env.VITE_API_URL
 
+console.log("API URL:", BASE_URL) // Debug ke liye
+
+export const API = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true
+})
 // Attach JWT from localStorage (keeps parity with previous utils/api behaviour)
 API.interceptors.request.use(
   (config) => {
